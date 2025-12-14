89e86719433fb06825c24f9549b94a10b8b5be8a
Wilfred Osozi <osoziw@gmail.com>
2025-12-14 16:54:45 +0300

    add error boundary

---
+++ MySQLServer.py
@@ -0,0 +1,25 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    mydb = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "@W28395872o",
+        database = "alx_book_store"
+    )
+
+    if mydb.is_connected():
+        mycursor = mydb.cursor()
+
+        mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
+
+except Error as e:
+        print(f"Error: {e}")
+
+finally:
+    if 'cursor' in locals() and mycursor:
+        mycursor.close()
+    if 'connection' in locals() and mydb.is_connected():
+        mydb.close()
